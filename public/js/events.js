97f63131bccc32fe9e6e22ecb585200e0701f438
Daniel Oswalt <oswalt@lclark.edu>
2017-12-13 12:38:10 -0800

    slideToggle

--- public/js/events.js
+++ public/js/events.js
@@ -2,8 +2,12 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     $('.slice-title').on('click', function(e) {
-      console.log($(e.target).parent().attr('data-index'));
-      
+      // console.log($(e.target).parent().attr('data-index'));
+      $(e.target).next().slideToggle(200);
     });
+
+    $('.slice-item-title').on('click', function(e) {
+      $(e.target).next().slideToggle(200);
+    })
 
 });
